f08365e0c322245bc4c34ef67cb98818d93c9603
stevehowse <s.howse2@googlemail.com>
2013-09-08 11:12:37 +0100

    edited migrations

--- db/migrate/20130907090950_change_date_format.rb
+++
@@ -1,9 +0,0 @@
-class ChangeDateFormat < ActiveRecord::Migration
-  def up
-rename_column :contents, :weekbg, :weekbg_id
-
-  end
-
-  def down
-  end
-end
--- db/migrate/20130907093738_change_userreqs.rb
+++
@@ -1,8 +0,0 @@
-class ChangeUserreqs < ActiveRecord::Migration
-  def up
-rename_column :userreqs, :weekbg, :weekbg_id
-  end
-
-  def down
-  end
-end
