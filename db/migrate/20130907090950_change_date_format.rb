055a7d5cab3936c4a7201777079ee22c033dac5b
stevehowse <s.howse2@googlemail.com>
2013-09-08 09:45:26 +0100

    Change database

--- db/migrate/20130907090950_change_date_format.rb
+++ db/migrate/20130907090950_change_date_format.rb
@@ -1,7 +1,7 @@
 class ChangeDateFormat < ActiveRecord::Migration
   def up
 rename_column :contents, :weekbg, :weekbg_id
-ALTER TABLE :contents ALTER COlUMN :weekbg_id TYPE :integer 
+
   end
 
   def down
--- db/migrate/20130907093738_change_userreqs.rb
+++ db/migrate/20130907093738_change_userreqs.rb
@@ -1,7 +1,6 @@
 class ChangeUserreqs < ActiveRecord::Migration
   def up
 rename_column :userreqs, :weekbg, :weekbg_id
-change_column :userreqs, :weekbg_id, :integer
   end
 
   def down
